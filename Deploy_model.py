0d164fe5b0abc554dd7bd9df9f061c5bf1d683ac
Abhirup Peeyal Sinha <67280401+abhiruppeeyalsinha@users.noreply.github.com>
2022-09-14 15:20:47 +0530

    Update Deploy_model.py

--- Deploy_model.py
+++ Deploy_model.py
@@ -4,23 +4,6 @@ from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
-# index_file = r"E:\Projects & Tutorial\CNN Project\Hyperparameter Tuning a Neural Network\templates\index.html"
-
-# model_path = r"E:\Projects & Tutorial\CNN Project\Hyperparameter Tuning a Neural Network\model file\hyper_parameter tuning"
-# list_model = os.listdir(model_path)
-# for i in list_model:
-#     print(i)
-#     if i == "dt_clf!!.sav":
-#         load_model = pickle.load(open(os.path.join(model_path, i), "rb"))
-#         # result_ = load_model.score(x_test, y_test)
-#         # result_ = (np.around(result_,2))*100
-#         # print(f"{int(result_)}%")
-#     else:
-#         load_model = pickle.load(open(os.path.join(model_path, i), "rb"))
-# result = (np.round(accuracy_score(y_test, load_model), 2))
-# result = (np.round(result,2))*100
-# result= int(result)
-# print(f"{result}%")
 
 model = pickle.load(open("dt_clf!!.sav", "rb"))
 
